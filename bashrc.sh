41b8097940a7b344c7981c8d6fc630312ebcbbe3
wuaoxiang <wuaoxiang@stargraph.cn>
2021-12-31 18:01:35 +0800

    udpate vscode settings

--- bashrc.sh
+++ bashrc.sh
@@ -12,14 +12,6 @@ export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 
 ## alias
 
-### alias change keyboard
-: <<'BLOCK_COMMENT'
-alias caps="hidutil property --set '{\"UserKeyMapping\":[{\"HIDKeyboardModifierMappingSrc\":0x700000039,\"HIDKeyboardModifierMappingDst\":0x70000002A}]}'"
-alias uncaps="hidutil property --set '{\"UserKeyMapping\":[{\"HIDKeyboardModifierMappingSrc\":0x700000039,\"HIDKeyboardModifierMappingDst\":0x700000039}]}'"
-alias disable_middle_button="xinput set-button-map 17 1 0 3"
-alias enable_middle_button="xinput set-button-map 17 1 2 3"
-BLOCK_COMMENT
-
 ### alias command shortcut
 alias ec="expressvpn connect"
 alias edd="expressvpn disconnect"
@@ -28,3 +20,11 @@ alias ju="journalctl --user --output cat"
 alias gitac="git add . && git commit -m \"$(date +'%x %X')\""
 # use `github.com.cnpmjs.org` instead?
 alias gitproxy="all_proxy=socks5://127.0.0.1:41537 git"
+
+### deprecated/macos alias
+: <<'BLOCK_COMMENT'
+alias caps="hidutil property --set '{\"UserKeyMapping\":[{\"HIDKeyboardModifierMappingSrc\":0x700000039,\"HIDKeyboardModifierMappingDst\":0x70000002A}]}'"
+alias uncaps="hidutil property --set '{\"UserKeyMapping\":[{\"HIDKeyboardModifierMappingSrc\":0x700000039,\"HIDKeyboardModifierMappingDst\":0x700000039}]}'"
+alias disable_middle_button="xinput set-button-map 17 1 0 3"
+alias enable_middle_button="xinput set-button-map 17 1 2 3"
+BLOCK_COMMENT
